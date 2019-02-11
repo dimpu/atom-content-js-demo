ead84c0714d256f140e64c6429d43a12109a8e88
Dimpu <buddhaaravind@gmail.com>
2019-02-11 18:45:13 -0500

    INIT

---
+++ README.md
@@ -0,0 +1,9 @@
+#Atom Content JS demo
+
+
+```shell
+npm i
+npm start
+
+```
+
---
+++ env.js
@@ -0,0 +1,19 @@
+window.appConfig = {
+    'config': {
+    'auth': {
+      'gateway': {
+        'cookie': 'atom_maestro_session'
+      }
+    },
+    'services': {
+      'publisher': {
+        'host': 'https://publisher-integration.qa.ktp.io',
+        'api': '/api/v2/',
+        'index': {
+          'content': 'content_integration'
+        }
+      }
+    }
+  }
+};
+ 
